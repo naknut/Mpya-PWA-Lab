e16e27dbadce0aaff78e140f0fb5dd12bda63152
Marcus Isaksson <me@naknut.email>
2023-12-07 15:09:02 +0100

    Add Service Worker

--- index.js
+++ index.js
@@ -1,6 +1,8 @@
 const allowButton = document.getElementById("allow")
 const notificationButton = document.getElementById("notification")
 
+navigator.serviceWorker.register("sw.js");
+
 allowButton.addEventListener("click", () => {
     Notification.requestPermission().then((result) => {
         console.log(result)
